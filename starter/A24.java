14f9de3cb319006a768ca96044eeb1eb46ace403
codytheking <codytheking93@gmail.com>
2021-01-06 10:49:39 -0800

    Starter code from in class

---
+++ starter/A24.java
@@ -0,0 +1,28 @@
+
+public class A24
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static int biggestDiff(String str)
+    {
+        int min = 9;
+        int max = 0;
+
+        // for loop (go through each char in the string)
+        {
+            String numChar = str.substring(i, i+1);
+            int num = Integer.parseInt(numChar);
+
+            // check if num is smaller than current min
+                // if so, that is the new current min
+
+            // check if num is bigger than current max
+                // if so, that is the new current max
+        }
+
+        // return the difference between min and max
+    }
+}
